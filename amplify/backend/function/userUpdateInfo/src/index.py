ae3ca227702d1fe978661f9c2aaac0a4bfed0b98
Shubham Malhotra <sm9971@rit.edu>
2024-06-30 11:10:58 -0400

    testing

--- amplify/backend/function/userUpdateInfo/src/index.py
+++ amplify/backend/function/userUpdateInfo/src/index.py
@@ -1,15 +1,31 @@
-import json
+from flask import Flask, jsonify, request
+import boto3
+from flask_cors import CORS
+from boto3.dynamodb.conditions import Key
+import requests
+import awsgi  # Import awsgi module for AWS Lambda integration
 
-def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('userDatabaseDDB-dev')
+
+# Base route for API endpoints
+BASE_ROUTE = "/updateUserInfo"
+
+app = Flask(__name__)
+CORS(app)
+
+# Endpoint to handle POST requests for saving form data
+@app.route(BASE_ROUTE, methods=["POST"])
+def postToDB():
+
+        return jsonify({'message': 'Data stored successfully'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+        
+    
+
+def handler(event,context):
+    
+    return awsgi.response(app,event,context)
